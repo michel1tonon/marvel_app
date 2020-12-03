a49b2200ac15850ba053ea3e40741fae7298e0e3
Michel Tonon <michel.tonon@stoom.com.br>
2020-12-03 09:17:04 -0300

    [Melhoria] utilizando método de acesso correto ao mocks.

--- lib/app/shared/helpers/utils.dart
+++ lib/app/shared/helpers/utils.dart
@@ -1,19 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/services.dart';
 
-String loadMockFromAssets(String fileName) {
-  try {
-    // env test or dev.
-    String dir = Directory.current.path;
-    if (dir.endsWith('/test')) {
-      dir = Directory.current.parent.path;
-    }
-    return File('$dir/assets/mocks/$fileName.json').readAsStringSync();
-  } catch (e) {
-    print('error $e');
-  }
-  return null;
+Future<String> loadMockFromAssets(String nameFile) async {
+  return await rootBundle.loadString('assets/mocks/$nameFile.json');
 }
 
 Future<void> applyPortrait(){
